dc4c07556412cbd2c1611bce8d0a25b75bb5afed
Amin <aminntfnd@gmail.com>
2026-07-22 04:03:57 +0330

    update test script to run tests on nodejs

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
-set -xe
+set -e
+
+echo "-- Testing on Erlang VM:"
 rm -rf ./build/test
 mkdir -p ./build/test
-
-echo "Testing on Erlang VM"
 gleam test
+
+echo -e "\n-- Testing on NodeJs:"
+rm -rf ./build/test
+mkdir -p ./build/test
+gleam test -t javascript --runtime nodejs
